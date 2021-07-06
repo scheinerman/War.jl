3c9219323082af9ec42096824a4f7aff9f88c7b5
Ed Scheinerman <ers@jhu.edu>
2021-07-05 20:22:24 -0400

    Initial working version

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # War
-Computer plays the card game war with itself.
+
+This is a demonstration project in which the computer plays the card game war with itself. 
+
+The primary function is `play_war()` which plays one full game. A player loses when s/he runs out of cards. In this version, if there is a war, and a player does not hold 4 cards (to play three face down and then one face up), that player loses. Not everyone plays that way.
+
+After each battle, the victor acquires the cards that were played; they are added to the bottom of the victor's pile in random order.
+
+The output of `play_war()` is vector containing the number of cards held by the first player at each turn. That vector necessarily begins with the number 26 and ends with either 0 or 52. The length of the vector is the number of battles to complete the game. In a normal battle the difference between consecutive terms is ±1; after an ordinary war, the difference is ±5; after a double war, the difference is ±9, and so forth. 
+
+## Verbose Output
+
+The function `verbose_war` may be used to control output during a game. With `verbose_war(true)` extensive output is produced:
+
+
+```julia
+julia> verbose_war(true)
+true
+
+julia> play_war();
+[ Q♢ K♣ K♠ 5♡ 2♢ 5♣ 5♢ 9♡ 9♠ 8♠ 8♣ 4♢ 9♢ 2♣ 3♢ T♡ 7♣ T♠ J♣ Q♣ A♣ 4♠ 4♡ 8♢ T♣ 7♠ 4♣ ]
+[ T♢ 3♠ 6♣ 6♠ 2♡ K♡ J♢ 2♠ 6♢ A♠ 3♣ A♢ 7♡ 7♢ A♡ J♡ 5♠ K♢ 8♡ 3♡ Q♡ 9♣ J♠ Q♠ 6♡ ]
+
+[ K♣ K♠ 5♡ 2♢ 5♣ 5♢ 9♡ 9♠ 8♠ 8♣ 4♢ 9♢ 2♣ 3♢ T♡ 7♣ T♠ J♣ Q♣ A♣ 4♠ 4♡ 8♢ T♣ 7♠ 4♣ Q♢ T♢ ]
+[ 3♠ 6♣ 6♠ 2♡ K♡ J♢ 2♠ 6♢ A♠ 3♣ A♢ 7♡ 7♢ A♡ J♡ 5♠ K♢ 8♡ 3♡ Q♡ 9♣ J♠ Q♠ 6♡ ]
+
+[ K♠ 5♡ 2♢ 5♣ 5♢ 9♡ 9♠ 8♠ 8♣ 4♢ 9♢ 2♣ 3♢ T♡ 7♣ T♠ J♣ Q♣ A♣ 4♠ 4♡ 8♢ T♣ 7♠ 4♣ Q♢ T♢ K♣ 3♠ ]
+[ 6♣ 6♠ 2♡ K♡ J♢ 2♠ 6♢ A♠ 3♣ A♢ 7♡ 7♢ A♡ J♡ 5♠ K♢ 8♡ 3♡ Q♡ 9♣ J♠ Q♠ 6♡ ]
+
+[ 5♡ 2♢ 5♣ 5♢ 9♡ 9♠ 8♠ 8♣ 4♢ 9♢ 2♣ 3♢ T♡ 7♣ T♠ J♣ Q♣ A♣ 4♠ 4♡ 8♢ T♣ 7♠ 4♣ Q♢ T♢ K♣ 3♠ K♠ 6♣ ]
+[ 6♠ 2♡ K♡ J♢ 2♠ 6♢ A♠ 3♣ A♢ 7♡ 7♢ A♡ J♡ 5♠ K♢ 8♡ 3♡ Q♡ 9♣ J♠ Q♠ 6♡ ]
+
+[ 2♢ 5♣ 5♢ 9♡ 9♠ 8♠ 8♣ 4♢ 9♢ 2♣ 3♢ T♡ 7♣ T♠ J♣ Q♣ A♣ 4♠ 4♡ 8♢ T♣ 7♠ 4♣ Q♢ T♢ K♣ 3♠ K♠ 6♣ ]
+[ 2♡ K♡ J♢ 2♠ 6♢ A♠ 3♣ A♢ 7♡ 7♢ A♡ J♡ 5♠ K♢ 8♡ 3♡ Q♡ 9♣ J♠ Q♠ 6♡ 5♡ 6♠ ]
+
+It's WAR!
+[ 8♠ 8♣ 4♢ 9♢ 2♣ 3♢ T♡ 7♣ T♠ J♣ Q♣ A♣ 4♠ 4♡ 8♢ T♣ 7♠ 4♣ Q♢ T♢ K♣ 3♠ K♠ 6♣ 5♣ 5♢ 2♡ K♡ 2♠ J♢ 6♢ 2♢ 9♠ 9♡ ]
+[ A♠ 3♣ A♢ 7♡ 7♢ A♡ J♡ 5♠ K♢ 8♡ 3♡ Q♡ 9♣ J♠ Q♠ 6♡ 5♡ 6♠ ]
+```
+and so on until
+```julia
+[ 3♣ T♠ 8♢ 9♡ 2♢ ]
+[ 3♡ 7♣ K♣ 3♠ Q♣ Q♠ 4♣ T♡ 8♡ A♢ K♠ 9♢ T♣ 4♡ J♡ 6♣ 5♡ 6♠ 4♠ 9♠ J♣ 5♢ 6♡ J♠ A♠ 5♣ 7♠ Q♡ 7♡ A♡ 2♣ J♢ 2♠ A♣ 4♢ K♢ Q♢ T♢ 2♡ 5♠ K♡ 8♠ 3♢ 8♣ 7♢ 9♣ 6♢ ]
+
+It's WAR!
+[ ]
+[ Q♠ 4♣ T♡ 8♡ A♢ K♠ 9♢ T♣ 4♡ J♡ 6♣ 5♡ 6♠ 4♠ 9♠ J♣ 5♢ 6♡ J♠ A♠ 5♣ 7♠ Q♡ 7♡ A♡ 2♣ J♢ 2♠ A♣ 4♢ K♢ Q♢ T♢ 2♡ 5♠ K♡ 8♠ 3♢ 8♣ 7♢ 9♣ 6♢ 3♠ 8♢ 3♡ Q♣ 9♡ 7♣ 3♣ 2♢ T♠ K♣ ]
+```
+In this game, player #1 has lost. 
--- src/War.jl
+++ src/War.jl
@@ -1,26 +1,43 @@
 module War
 
+using DataStructures: isempty, collect, popfirst!
 using PlayingCards52, DataStructures
 
 import Base: show
 
-export Position
+export Position, battle, play_war, verbose_war
 
-greet() = print("Hello World!")
+ace_high()
+
 
 Pile = Deque{Card}
 
+
+"""
+    make_pile(clist::Vector(Card))
+
+Make a `Pile` from a list of cards. 
+"""
 function make_pile(clist::Vector{Card})::Pile
     result = Pile()
+    add_to_pile(result, clist)
+    return result
+end
+
+"""
+    add_to_pile(pile::Pile, clist::Vector{Card})
+
+Add the cards in `clist` to the bottom of `pile`.
+"""
+function add_to_pile(pile::Pile, clist::Vector{Card})
     for c in clist
-        push!(result,c)
+        push!(pile, c)
     end
-    return result 
 end
 
 function _string(pile::Pile)::String
     result = "[ "
-    for c in pile 
+    for c in pile
         result *= "$c "
     end
     result *= "]"
@@ -38,8 +55,121 @@ end
 Position() = Position(deck())
 
 function show(io::IO, P::Position)
-    println(io,_string(P.A))
-    println(io,_string(P.B))
+    println(io, _string(P.A))
+    println(io, _string(P.B))
 end
 
-end # module
+const A_win = -1
+const B_win = 1
+const Tie = 0
+
+VERBOSE = false
+
+function verbose_war(tf::Bool = true)
+    global VERBOSE = tf
+end
+
+
+
+"""
+    battle_compare(a::Card, b::Card)::Int 
+
+Compare two cards by War rules. Returns:
+* `A_win` if `a` beats `b`.
+* `B_win` if `b` beats `a`.
+* `Tie` if `a` and `b` have the same rank.
+"""
+function battle_compare(a::Card, b::Card)::Int
+    if rank(a) == rank(b)
+        return Tie
+    end
+    return a > b ? A_win : B_win
+end
+
+
+function give_spoils(P::Position, used::Set{Card}, winner::Int)
+    spoils = shuffle(collect(used))
+    if winner == A_win
+        add_to_pile(P.A, spoils)
+    end
+    if winner == B_win
+        add_to_pile(P.B, spoils)
+    end
+    nothing
+end
+
+
+"""
+    battle(P::Position, used::Set{Card})
+
+execute one turn in War.
+"""
+function battle(P::Position, used::Set{Card} = Set{Card}())
+
+    # if a player is out of cards, the spoils go the other player (who then wins)
+    if isempty(P.A) || isempty(P.B)
+        result = isempty(P.A) ? B_win : A_win
+        give_spoils(P, used, result)
+        return
+    end
+
+    # get the first card from each player and compare
+    a = popfirst!(P.A)
+    b = popfirst!(P.B)
+    push!(used, a)
+    push!(used, b)
+
+    result = battle_compare(a, b)
+    if result != Tie
+        give_spoils(P, used, result)
+        return
+    end
+
+    if VERBOSE
+        println("It's WAR!")
+    end
+
+    # need at least 4 cards or the game is lost
+
+    if length(P.A) < 4      # A loses
+        while !isempty(P.A)
+            c = popfirst!(P.A)
+            push!(used, c)
+        end
+        give_spoils(P, used, B_win)
+        return
+    end
+
+    if length(P.B) < 4      # B loses
+        while !isempty(P.B)
+            c = popfirst!(P.B)
+            push!(used, c)
+        end
+        give_spoils(P, used, A_win)
+        return
+    end
+
+    # both have enough cards 
+    for k = 1:3
+        c = popfirst!(P.A)
+        push!(used, c)
+        c = popfirst!(P.B)
+        push!(used, c)
+    end
+    battle(P, used)
+end
+
+function play_war(d::Vector{Card} = deck())
+    P = Position(d)
+    result = [26]
+    while length(P.A) > 0 && length(P.B) > 0
+        battle(P)
+        if VERBOSE
+            println(P)
+        end
+        push!(result, length(P.A))
+    end
+    return result
+end
+
+end  #end of module War
