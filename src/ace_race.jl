84f55e9c9a1923a089ac34365af64d2abe71f319
Ed Scheinerman <ers@jhu.edu>
2021-07-07 16:59:35 -0400

    Allow start with four kings &c

--- src/ace_race.jl
+++ src/ace_race.jl
@@ -5,14 +5,20 @@ and the other player has the rest of the deck, how do they fare?
 
 using PlayingCards52, War, ProgressMeter
 
-function ace_race(reps::Int = 100_000)
-    index_set = Set(2:13) ∪ Set(15:26) ∪ Set(28:39) ∪ Set(41:52)
-    d1 = Card.([1, 14, 27, 40])
-    d2 = Card.(collect(index_set))
+"""
+    ace_race(reps::Int = 10_000)
+
+returns the frequency that four aces vs the rest of the deck wins at war.
+Optionally, `ace_race(reps, skip_rank::Int)` to skip a different rank, e.g.,
+`ace_race(skip_rank=13)` for kings.
+"""
+function ace_race(reps::Int = 10_000; skip_rank = 1)
+
+    d = deck(false)
+    d2 = [c for c in d if rank(c) != skip_rank]
+    d1 = [Card(s, skip_rank) for s in PlayingCards52.suit_list]
 
     a_wins = 0
-    a_steps = 0
-    b_steps = 0
     PM = Progress(reps)
 
     for k = 1:reps
@@ -20,19 +26,18 @@ function ace_race(reps::Int = 100_000)
         r = play_war(d1, d2)
         if r[end] > 0
             a_wins += 1
-            a_steps += length(r) - 1
-        else
-            b_steps += length(r) - 1
         end
         next!(PM)
     end
 
-    a_pct = 100a_wins / reps
-    a_len = a_steps / reps
-    b_len = b_steps / reps
+    a_pct = a_wins / reps
+    return a_pct
 
-    println("Four aces wins $a_pct% of the time")
-    println("Average length of a winning game: $a_len")
-    println("Average length of a losing game:  $b_len")
+    # a_len = a_steps / reps
+    # b_len = b_steps / reps
+
+    # println("Four aces wins $a_pct% of the time")
+    # println("Average length of a winning game: $a_len")
+    # println("Average length of a losing game:  $b_len")
 
 end
